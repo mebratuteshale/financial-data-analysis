612daf9c3e6cb973b81713e32cb778b0052034bd
Mebratu <mebratuteshalew@gmail.com>
2024-12-17 12:04:28 +0300

    RSI, EMA, MACD calcualation using ta-lib

--- scripts/financial_analyzer.py
+++ scripts/financial_analyzer.py
@@ -17,10 +17,13 @@ class FinancialAnalyzer:
         self.ticker=ticker
         self.start_date=start_date
         self.end_date=end_date
+
     def retrieve_stock_data(self):
         return yf.download(self.ticker,start=self.start_date,end=self.end_date)
+    
     def calculate_moving_average(self,data,window_size):
         return ta.SMA(data,timeperiod=window_size)
+    
     def calculate_technical_indicators(self,data):
         # calculate various techical indicators
         data['SMA']=self.calculate_moving_average(data['Close'],20)
@@ -31,17 +34,22 @@ class FinancialAnalyzer:
         data['MACD_Signal']=macd_signal
         # Add more indicators as needed
         return data
-    def plot_stock_data(self,data):
-        fig = px.line(data,x=data.index,y=['Close','SMA'],title='Stock Price with Moving')
+    
+    def plot_stock_data(self,data,theYaxis:list=['Close','SMA'],title:str='Stock Price with Moving'):
+        fig = px.line(data,x=data.index,y=theYaxis,title=title)
         fig.show()
+
     def plot_rsi(self,data):
         fig=px.line(data,x=data.index,y='RSI',title='Relative Strength Index (RSI)')
         fig.show
+
     def plot_ema(self,data):
         fig=px.line(data,x=data.index,y=['Close','EMA'],title='Stock Price with Exponent')
         fig.show()
+
     def plot_macd(self,data):
         fig=px.line(data,x=data.index,y=['MACD','MACD_Signal'],title='Moving Average ')
+
     def calculate_portfolio_weight(self,tickers,start_date,end_date):
         data=yf.download(tickers,start=start_date,end=end_date)['Close']
         mu=expected_returns.mean_historical_return(data)
@@ -50,6 +58,7 @@ class FinancialAnalyzer:
         weights=ef.max_sharpe()
         weights=dict(zip(tickers,weights.values()))
         return weights
+    
     def calculate_portfolio_performance(self,tickers,start_date,end_date):
         data=yf.download(tickers,start=start_date,end=end_date)['Close']
         mu=expected_returns.mean_historical_return(data)
@@ -58,6 +67,7 @@ class FinancialAnalyzer:
         weights=ef.max_sharpe()
         portfolio_return, portfolio_volatility, sharpe_ratio=ef.portfolio_performance()
         return portfolio_return, portfolio_volatility,sharpe_ratio
+ 
  
 class PlotGraph:
     def plot_stock_data(dataFrame,title:str='Stock Value Over Time'):
